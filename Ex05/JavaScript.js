d19146ec1c1bf8f6027f9dcff7e54fe59ba72f7d
MarcosRangelRZanoni <99424764+MarcosRangelRZanoni@users.noreply.github.com>
2022-09-27 19:40:34 -0300

    comi

--- Ex05/JavaScript.js
+++ Ex05/JavaScript.js
@@ -1,27 +1,27 @@
-let btnAdicionar = document.getElementById("btnAdicionar");
-let valores = document.getElementById("valores");
-let campoValor = document.querySelector('.campoValor');
-campoValor.focus();
 let elementos = [];
-let elementoOL = document.createElement("OL");
-elementoOL.id = "minhaLista";
-let p = document.getElementById("valores");
-let elementLI = document.createElement("LI");
+let btnAdicionar = document.getElementById("btnAdicionar");
 
 btnAdicionar.addEventListener("click", function () {
-    let valor = document.getElementById("valor").value;
-    elementos.push(valor);
-    // ordenar o array
-    console.log(elementos);
-   
-   
-    elementos.forEach(element => {
-        let textNode = document.createTextNode(element);
-        elementLI.appendChild(textNode);
 
-        elementoOL.appendChild(elementLI);
-        campoValor.value = '';
-        campoValor.focus();
+    let valor = document.getElementById("valores").value;
+    elementos.push(valor.toString());
+
+    let oldOL = document.getElementById("list");
+    if (oldOL !== null)
+   { document.body.removeChild(oldOL); }
+
+    elementos.sort();
+
+    let elementoOL = document.createElement("OL");
+    elementoOL.setAttribute("id", "list");
+    elementos.forEach(element => {
+        let elementoLI = document.createElement("LI");
+        let elementBR = document.createElement("BR");
+        let textNode = document.createTextNode(element);
+        elementoLI.appendChild(textNode);
+        elementoOL.appendChild(textNode);
+        elementoOL.appendChild(elementBR);
     });
-    p.appendChild(elementoOL);
-}); 
+
+    document.body.appendChild(elementoOL);
+});
